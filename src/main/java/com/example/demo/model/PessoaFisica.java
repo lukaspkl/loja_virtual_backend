16970ec79b599a645130feb5fd274e1bd09544f9
lukaspkl <fohrenoficial@Live.com>
2024-03-21 18:50:06 -0300

    Commit Classes Pessoa_Fisica_Juridica

---
+++ src/main/java/com/example/demo/model/PessoaFisica.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(nullable = false)
+	private String cpf;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimnento;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimnento() {
+		return dataNascimnento;
+	}
+
+	public void setDataNascimnento(Date dataNascimnento) {
+		this.dataNascimnento = dataNascimnento;
+	}
+
+
+
+	
+	
+
+}
